f4bed8c180c11c27647dfb40f60abcbf8dacd54b
USAMI Kenta <tadsan@zonu.me>
2021-03-14 21:49:19 +0900

    Impl ChronosClock class

---
+++ src/ChronosClock.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bag2\Clock;
+
+use Cake\Chronos\Chronos;
+use DateTimeImmutable;
+use PsrProposal\Clock\ClockInterface;
+
+class ChronosClock implements ClockInterface
+{
+    /**
+     * @return Chronos
+     */
+    public function now(): DateTimeImmutable
+    {
+        return new Chronos();
+    }
+}
